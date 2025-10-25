04e2cb5eb67f35da09d08032d188492ac213de6f
Gaurav Shambharkar <shambharkargaurav04@gmail.com>
2025-10-25 21:44:00 +0530

    setHelmet

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 dotenv.config();
+import helmet from "helmet";
 
-const investa = express().use(express.json());
+const investa = express().use(express.json(), helmet());
+
+(() => {
+  if (!process.env.DATABASE_URL) {
+    return new Error("DATABASE_URL is missing from ");
+  }
+  mongoose.connect(process.env.DATABASE_URL);
+})();
 
 investa.use("/investa/v1/readStocks", (req, res) => {
-  res.send("Investa Backend is running");
+  res.send({
+    message: "Welcome to Investa API v1 - Read Stocks Endpoint",
+  });
 });
 
 const PORT = process.env.PORT || 5000;
